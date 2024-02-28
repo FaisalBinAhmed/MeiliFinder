268bf2102ceb7b7fb6c7b663019718351b429c6e
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-02-28 18:40:50 +0100

    Update config_handler.rs

--- src/utilities/config_handler.rs
+++ src/utilities/config_handler.rs
@@ -1,16 +1,22 @@
 // this file is used to read instances.json and settings.json files from the project root
 
 use std::fs::File;
+use serde::{Deserialize, Serialize};
+
 use crate::app::Instance;
 
+#[derive(Serialize, Deserialize)]
+pub struct Settings {
+    //tbd
+}
 
 
 pub fn retrieve_instances_from_file() -> Vec<Instance> {
 
 
     if let Ok(file) = File::open("instances.json") {
-        let reader = std::io::BufReader::new(file);
-        let instances: Vec<Instance> = match serde_json::from_reader(reader) {
+
+        let instances: Vec<Instance> = match serde_json::from_reader(file) {
             Ok(instances) => instances,
             Err(_) => vec![],
         };
@@ -18,4 +24,17 @@ pub fn retrieve_instances_from_file() -> Vec<Instance> {
     }
      vec![]
 
+}
+
+pub fn retrieve_settings_from_file() -> Settings {
+    //tbd
+    if let Ok(file) = File::open("settings.json") {
+        let settings: Settings = match serde_json::from_reader(file) {
+            Ok(settings) => settings,
+            Err(_) => Settings{},
+        };
+        settings
+    } else {
+        Settings{}
+    }
 }
