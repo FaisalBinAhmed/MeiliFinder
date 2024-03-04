009f8bcf4ba73322e5df157be6dc4491f43e8438
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-03-04 22:22:23 +0100

    Update input_bar.rs

--- src/components/input_bar.rs
+++ src/components/input_bar.rs
@@ -1,7 +1,9 @@
 use ratatui::{prelude::*, widgets::Paragraph};
 
-use crate::{app::{self, App}, Frame};
-
+use crate::{
+    app::{self, App},
+    Frame,
+};
 
 pub fn draw_input_bar(f: &mut Frame, chunk: Rect, app: &App) {
     let input_field: Text<'_> = match app.current_search_form {
@@ -19,26 +21,26 @@ pub fn draw_input_bar(f: &mut Frame, chunk: Rect, app: &App) {
     };
 
     let input_paragraph = Paragraph::new(input_field)
-    .block(ratatui::widgets::Block::default().borders(ratatui::widgets::Borders::ALL).title(title).border_style(Style::default().fg(Color::LightGreen)))
-    // .style(Style::default().fg(ratatui::style::Color::Black).bg(ratatui::style::Color::White))
+        .block(
+            ratatui::widgets::Block::default()
+                .borders(ratatui::widgets::Borders::ALL)
+                .title(title)
+                .border_style(Style::default().fg(Color::LightGreen)),
+        )
+        // .style(Style::default().fg(ratatui::style::Color::Black).bg(ratatui::style::Color::White))
         .alignment(ratatui::prelude::Alignment::Left);
 
     f.render_widget(input_paragraph, chunk);
     set_cursor_position(f, app, chunk);
 }
 
+// we can put the cursor in the right position by offsetting the input bar text size  TODO:
+fn set_cursor_position(f: &mut Frame, app: &App, chunk: Rect) {
+    let cursor_position = match app.current_search_form {
+        crate::app::SearchForm::Query => app.cursor_position,
+        crate::app::SearchForm::Filter => app.filter_cursor_position,
+        crate::app::SearchForm::Sort => app.sort_cursor_position,
+    };
 
-
-    // we can put the cursor in the right position by offsetting the input bar text size  TODO: 
-    fn set_cursor_position(f: &mut Frame, app: &App, chunk: Rect) {
-        let cursor_position = match app.current_search_form {
-            crate::app::SearchForm::Query => app.cursor_position,
-            crate::app::SearchForm::Filter => app.filter_cursor_position,
-            crate::app::SearchForm::Sort => app.sort_cursor_position,
-        };
-
-        f.set_cursor(
-            chunk.x + cursor_position as u16 + 1,
-            chunk.y + 1,
-        );
-    }
+    f.set_cursor(chunk.x + cursor_position as u16 + 1, chunk.y + 1);
+}
