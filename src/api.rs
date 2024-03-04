ffe0669e340ffa16ee0ef6a0d7e47705706f0d66
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-03-04 22:16:02 +0100

    Update api.rs

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,10 @@
-use meilisearch_sdk::{client, documents, Client, DocumentsQuery, DocumentsResults, Index, IndexesQuery, IndexesResults, SearchResults, Settings, Task, TaskInfo, TasksResults};
+use meilisearch_sdk::{
+    client, documents, Client, DocumentsQuery, DocumentsResults, Index, IndexesQuery,
+    IndexesResults, SearchResults, Settings, Task, TaskInfo, TasksResults,
+};
 use serde_json::Value;
 
 use crate::app;
-
 
 pub struct TaskId {
     pub id: u32,
@@ -16,13 +18,13 @@ impl AsRef<u32> for TaskId {
 
 // for now
 pub fn get_client() -> Client {
-    Client::new("http://localhost:7700", Some("ZL4dOFgqygBrAGPapWs2LdgTSdveZ8qdsWTyBlyF9-M"))
+    Client::new(
+        "http://localhost:7700",
+        Some("ZL4dOFgqygBrAGPapWs2LdgTSdveZ8qdsWTyBlyF9-M"),
+    )
 }
 
-
 pub async fn search_documents(query: &str, filter: &str, sort: &str, index: &Index) -> Vec<Value> {
-
-
     let search_result: Result<SearchResults<Value>, _> = index
         .search()
         .with_query(query)
@@ -32,14 +34,16 @@ pub async fn search_documents(query: &str, filter: &str, sort: &str, index: &Ind
         .await;
 
     let documents: Vec<Value> = match search_result {
-        Ok(search_result) => search_result.hits.iter().map(|hit| hit.result.clone()).collect(),
-        Err(_) => vec![]
+        Ok(search_result) => search_result
+            .hits
+            .iter()
+            .map(|hit| hit.result.clone())
+            .collect(),
+        Err(_) => vec![],
     };
 
     documents
-
 }
-
 
 pub async fn get_tasks() -> Vec<Task> {
     // todo: get the Instance info from the app state
@@ -48,31 +52,27 @@ pub async fn get_tasks() -> Vec<Task> {
 
     match tasks_result {
         Ok(tasks) => return tasks.results,
-        Err(_) => return  vec![]
+        Err(_) => return vec![],
     }
-
 }
 
 pub async fn get_documents() -> Vec<Value> {
-
     let client = get_client(); //temp
 
     let movies = client.index("movies");
 
     let documents = DocumentsQuery::new(&movies)
-  .with_limit(20)
-  .execute::<Value>()
-  .await;
+        .with_limit(20)
+        .execute::<Value>()
+        .await;
 
-  match documents {
-      Ok(documents) => return documents.results,
-      Err(_) => return vec![]
-  }
-
+    match documents {
+        Ok(documents) => return documents.results,
+        Err(_) => return vec![],
+    }
 }
 
 pub async fn get_task_by_id_meili(task_id: TaskId, client: &Client) -> Option<Task> {
-
     let task = client.get_task(task_id).await;
 
     match task {
@@ -81,11 +81,7 @@ pub async fn get_task_by_id_meili(task_id: TaskId, client: &Client) -> Option<Ta
     }
 }
 
-
-
-
 pub async fn get_task_by_id(task_id: u32) -> Option<String> {
-    
     let master_key = "MASTER_KEY";
 
     let url = format!("http://localhost:7700/tasks/{}", task_id);
@@ -93,7 +89,10 @@ pub async fn get_task_by_id(task_id: u32) -> Option<String> {
     let client = reqwest::Client::new();
     let response = client
         .get(&url)
-        .header(reqwest::header::AUTHORIZATION, format!("Bearer {}", master_key))
+        .header(
+            reqwest::header::AUTHORIZATION,
+            format!("Bearer {}", master_key),
+        )
         .send()
         .await;
 
@@ -104,32 +103,23 @@ pub async fn get_task_by_id(task_id: u32) -> Option<String> {
         }
         Err(_) => return None,
     }
-
-
-
-
 }
 
-
 pub async fn get_all_indices() -> Vec<Index> {
-
     let client = get_client();
-    
+
     let indices_result: Result<IndexesResults, _> = IndexesQuery::new(&client)
-    // .with_limit(3)
-    .execute()
-    .await;
+        // .with_limit(3)
+        .execute()
+        .await;
 
     match indices_result {
         Ok(indices_result) => return indices_result.results,
-        Err(_) => return vec![]
+        Err(_) => return vec![],
     }
-
 }
 
-
 pub async fn get_all_index_settings() -> Vec<Settings> {
-
     let indices = get_all_indices().await;
 
     let mut settings: Vec<Settings> = vec![];
@@ -143,25 +133,15 @@ pub async fn get_all_index_settings() -> Vec<Settings> {
     }
 
     settings
-
 }
 
-
-
 pub async fn delete_document(index_uid: &str, document_id: &str) {
-
     let client = get_client();
 
-   let taskinfo_result = client
-  .index(index_uid)
-  .delete_document(document_id)
-  .await;
+    let taskinfo_result = client.index(index_uid).delete_document(document_id).await;
 
     match taskinfo_result {
         Ok(_taskinfo) => (),
-        Err(_) => ()
+        Err(_) => (),
     }
-
-
-    
 }
