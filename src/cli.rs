f2aa2f15b2062a36d12cf331b345c3d49cd50ea7
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-03-15 18:15:24 +0100

    static ascii

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
-use crate::app::Instance;
+use crate::{app::Instance, constants::APP_NAME_ASCII};
 
 use std::io::{self, BufRead};
 
@@ -15,12 +15,7 @@ pub fn prompt_user_for_instance_info() -> Result<Instance> {
         let mut host = String::new();
         let mut primary_key = String::new();
 
-
-        let logo = r#"╔╦╗┌─┐┬┬  ┬╔═╗┬┌┐┌┌┬┐┌─┐┬─┐
-║║║├┤ ││  │╠╣ ││││ ││├┤ ├┬┘
-╩ ╩└─┘┴┴─┘┴╚  ┴┘└┘─┴┘└─┘┴└─"#;
-
-        println!("{}", logo);
+        println!("{}", APP_NAME_ASCII);
 
         println!("Enter the name of the instance:");
         handle.read_line(&mut name)?;
@@ -36,8 +31,7 @@ pub fn prompt_user_for_instance_info() -> Result<Instance> {
             name: name.trim().to_string(),
             host: host.trim().to_string(),
             primary_key: primary_key.trim().to_string(),
+    })
 
-        })
-
-    }
+}
 
--- src/constants.rs
+++ src/constants.rs
@@ -4,3 +4,7 @@ use ratatui::style::Color;
 pub static INSTANCE_COLOR: Color = Color::Rgb(41, 174, 255);
 pub static INDEX_COLOR: Color = Color::Rgb(255, 205, 170);
 pub static ACTION_MODE_COLOR: Color = Color::Rgb(0, 135, 81);
+
+pub static APP_NAME_ASCII: &str = r#"╔╦╗┌─┐┬┬  ┬╔═╗┬┌┐┌┌┬┐┌─┐┬─┐
+║║║├┤ ││  │╠╣ ││││ ││├┤ ├┬┘
+╩ ╩└─┘┴┴─┘┴╚  ┴┘└┘─┴┘└─┘┴└─"#;
